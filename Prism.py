de0065b3f054813288307dc0d98c783aff3c566d
Naram <hh.naram@gmail.com>
2022-06-21 11:35:53 +0300

    Edge Detection

--- Prism.py
+++ Prism.py
@@ -1,5 +1,20 @@
+import cv2
+
 def main():
-    pass
+    imagePath = input("Image Path: ")
+    lowerThreshold = int(input("Lower Threshold: "))
+    upperThreshold = int(input("Upper Threshold: "))
+
+
+    image = cv2.imread(imagePath)
+    edges = cv2.Canny(image, lowerThreshold, upperThreshold)
+
+    cv2.imshow('Original', image)
+    cv2.imshow('Edge Detection', edges)
+
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
 
 if __name__ == '__main__':
     main()
