21cbf8b95656e950f8d410a1204ed32d08582563
Naram <hh.naram@gmail.com>
2022-06-21 12:43:22 +0300

    Edge Detection

--- Prism.py
+++ Prism.py
@@ -1,4 +1,5 @@
 import cv2
+import potrace
 
 def main():
     imagePath = input("Image Path: ")
@@ -11,6 +12,11 @@ def main():
 
     cv2.imshow('Original', image)
     cv2.imshow('Edge Detection', edges)
+
+    for i in range(len(edges)):
+        edges[i][edges[i] > 1] = 1
+    bitmap = potrace.Bitmap(edges)
+    path = bitmap.trace(2, potrace.TURNPOLICY_MINORITY, 1.0, 1, 0.5)
 
     cv2.waitKey(0)
     cv2.destroyAllWindows()
